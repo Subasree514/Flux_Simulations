2aa025b222a49bd22cd82be5970f529686744d93
Subasree514 <ssridhar@uni-koeln.de>
2025-01-29 10:03:24 +0100

    checked and removed long acyl coa

--- check_unused.py
+++ check_unused.py
@@ -24,9 +24,12 @@ from cobra.flux_analysis import production_envelope
 from cobra import Model, Reaction, Metabolite
 from cobra.flux_analysis import flux_variability_analysis
 import matplotlib.pyplot as plt
-from cobra.io import load_json_model, save_json_model, load_matlab_model, save_matlab_model, read_sbml_model, write_
+from cobra.io import load_json_model, save_json_model, load_matlab_model, save_matlab_model, read_sbml_model, write_sbml_model
 
-#core_model = cobra.io.load_matlab_model(join('alpha_day_DM.mat'))
 core_model = cobra.io.load_matlab_model(join('/home/subasree/Desktop/Models_to_work/model_rs.mat'))
-
-cobra.manipulation.delete.prune_unused_metabolites(core_model) 
+[core_model,rem]=cobra.manipulation.delete.prune_unused_metabolites(core_model)
+print(rem)
+model_rs=core_model
+save_matlab_model(model_rs, "/home/subasree/Desktop/Models_to_work/model_rs.mat")
+sol = core_model.optimize()
+print(core_model.summary(sol))
