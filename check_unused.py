147c8a68449f0a3937a1bcf75a9d0a7d8bd06230
Subasree514 <ssridhar@uni-koeln.de>
2025-01-29 09:50:17 +0100

    removemets

---
+++ check_unused.py
@@ -0,0 +1,32 @@
+
+from __future__ import division, print_function, absolute_import
+import csv
+import pandas
+from cobra.flux_analysis import (
+    single_gene_deletion, single_reaction_deletion, double_gene_deletion,
+    double_reaction_deletion)
+import os
+import xml.etree.ElementTree as etree
+import cobra
+import numpy as np
+from itertools import chain
+from cobra.util import solver as sutil
+from cobra.core.solution import get_solution
+from optlang.symbolics import add, Zero
+import pandas as pd
+import os
+from os.path import join
+import matplotlib.pyplot as plt
+from cobra.medium import minimal_medium
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from cobra.flux_analysis import production_envelope
+from cobra import Model, Reaction, Metabolite
+from cobra.flux_analysis import flux_variability_analysis
+import matplotlib.pyplot as plt
+from cobra.io import load_json_model, save_json_model, load_matlab_model, save_matlab_model, read_sbml_model, write_
+
+#core_model = cobra.io.load_matlab_model(join('alpha_day_DM.mat'))
+core_model = cobra.io.load_matlab_model(join('/home/subasree/Desktop/Models_to_work/model_rs.mat'))
+
+cobra.manipulation.delete.prune_unused_metabolites(core_model) 
